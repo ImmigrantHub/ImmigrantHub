e2d60fbf3eacd68148697a84a4f1aed6a94bd3e1
Kendrick Schuettler <keschuettler@gmail.com>
2017-07-15 09:31:04 -0400

    Menubar

--- src/public/app/User.jsx
+++ src/public/app/User.jsx
@@ -3,7 +3,10 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 import Divider from 'material-ui/Divider';
 import {List, ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
-
+import FlatButton from 'material-ui/FlatButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Chip from 'material-ui/Chip';
+import MenuBar from './MenuBar.jsx';
 const User = props => {
   return (
     <div className="User">
@@ -13,51 +16,60 @@ const User = props => {
         />
         <CardText>
             <List>
-              <ListItem primaryText="New York" />
+              <ListItem primaryText="Mexico City" />
               <Divider />
-              <ListItem primaryText="Boston" />
+              <ListItem primaryText="Mexico" />
               <Divider />
-              <ListItem primaryText="New Jersey" />
-              <Divider />
-              <ListItem primaryText="Maine" />
+              <ListItem primaryText="Latin America" />
             </List>
         </CardText>
       </Card>
       <Card className="User__content2">
          <CardHeader
            title="Kendrick"
-           subtitle="New York"
+           subtitle="Mexico City"
            avatar="https://avatars7.githubusercontent.com/u/8907103?v=4&s=460"
          />
          <CardTitle title="Story" subtitle="Track your progress" />
-        <Paper zDepth={2} rounded={false}> 
-          <div className="timeline">
-            <div className="timeline-bar"></div>
-            <div className="timeline-box-1"></div>
-            <div className="timeline-box-2"></div>
-            <div className="timeline-box-3"></div>
-          </div>
+        <Paper zDepth={2} rounded={false} className="timeline" > 
+            <ul className="timeline-list">
+              <li className="timeline-node">Step 1</li>
+              <li className="timeline-node">Step 2</li>
+              <li className="timeline-node">Step 3</li>
+              <li className="active">Step 4</li>
+              <li className="timeline-node">Step 5</li>
+            </ul>  
         </Paper>
          <CardText>
+           <h2>Step 5</h2>
+           <div className="chip-wrapper">
+            <Chip className="chip" key={0}> Legal </Chip>
+            <Chip className="chip" key={1}> Latin America </Chip>
+            <Chip className="chip" key={2}>DOCA</Chip>
+           </div>
            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
          </CardText>
+         <Divider/>
+         <CardActions>
+           <FlatButton label="Add New Step +" />
+         </CardActions>
+
+
       </Card>
       <Card className="User__content3">
         <CardHeader
-          title="Messages"
+          title="Message Inbox"
         />
         <CardText>
             <List>
-              <ListItem primaryText="Hello!" />
+              <ListItem primaryText="We can help!" />
               <Divider />
-              <ListItem primaryText="Wassup!" />
+              <ListItem primaryText="Call us!" />
               <Divider />
-              <ListItem primaryText="Hey!" />
-              <Divider />
-              <ListItem primaryText="Sup?" />
+              <ListItem primaryText="Hi Kendrick" />
             </List>
         </CardText>
       </Card>
