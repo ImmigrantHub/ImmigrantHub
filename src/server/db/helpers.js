ac1bb6d83d44de476f96f66e104d1f8ca957ee1e
dppedersen <pedersen.duncan@gmail.com>
2017-07-14 22:31:01 -0400

    Add Database Helpers

---
+++ src/server/db/helpers.js
@@ -0,0 +1,42 @@
+const Promise = require('bluebird');
+const db = require('./db-config.js');
+
+const helpers = {
+  create: (input, table, cb) => {
+    db.query(`INSERT INTO ${table} SET ?`, input, (err, res) => {
+      if(err) console.log(err);
+      console.log('inserted:', res);
+      cb(err, res)
+    })
+  },
+
+  retrieve: (query, cb) => {
+    db.query(query, (err, res)=> {
+      if(err) console.log(err);
+      cb(err, res)
+    })
+  },
+
+  update: (update, table, id, cb) => {
+    db.query(`UPDATE ${table} SET ? WHERE id=?`, [update, id], (err, res) => {
+      if(err) console.log(err);
+      console.log('updated:', res.changedRows, 'rows')
+      cb(err, res)
+    })
+  },
+
+  delete: (targetId, table, cb) => {
+    db.query(`DELETE FROM ${table} WHERE id=?`, targetId, (err, res) => {
+      if(err) console.log(err);
+      console.log('deleted:', res.affectedRows, 'rows')
+      cb(err, res)
+    })
+  }
+}
+
+module.exports = {
+  create: Promise.promisify(helpers.create),
+  retrieve: Promise.promisify(helpers.retrieve),
+  update: Promise.promisify(helpers.update),
+  delete: Promise.promisify(helpers.delete)
+}
