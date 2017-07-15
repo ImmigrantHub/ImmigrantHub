9d9834427f39bd53e7b56ba87d40ebf700be2faf
dppedersen <pedersen.duncan@gmail.com>
2017-07-15 08:24:58 -0400

    Organization

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-
-
 module.exports = (app, express) => {
 
   app.get('/', (req, res) => {
@@ -25,10 +21,3 @@ module.exports = (app, express) => {
   })
 
 };
-=======
-// const routes = require('express').Router();
-//
-// routes.get('/auth/github', passport.authenticate('github'));
-//
-// module.exports = routes;
->>>>>>> 4e6cb90ee8030264e966e9720c84a246520d2783
